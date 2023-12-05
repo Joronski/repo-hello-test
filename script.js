27524cb602b3390b70c8bda35e471659bb3726e6
Joronski <joronrecio@yahoo.com>
2023-12-05 20:08:16 +0800

    Added Features

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // Test
-console.log("Hello World!");
+console.log("Hello World!!!");
+
+var name = "Joron";
+var age = 19;
+
+console.log("My name is " + name + " and I am " + age + " years old!!!");
